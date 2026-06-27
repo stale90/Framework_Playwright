e4cecc6794d1bd0dcb0689eae1a16c3b262d4032
stale90 <english.shailendra@gmail.com>
2026-06-27 09:48:09 +0530

    first ngrok webhook push jenkins

--- src/test-scripts/PSYValidationTest.spec.ts
+++ src/test-scripts/PSYValidationTest.spec.ts
@@ -3,10 +3,11 @@ import { Helper } from "../rsk-modules/PSY/PSYHelper";
 
 test(`RSK(PSY) Verify Input validations: @psy`, async ({ page, pages, dataMap }) => {
   
+  // Step 1
   await test.step('New Form : Navigate to Application Form', async () => {
     await Helper.navigateToRegistrationForm(page, pages);
   });
-  
+  // Step 2
   await test.step('Validate Input Fields', async () => {
     await Helper.performFieldValidation(pages, dataMap);
   });
